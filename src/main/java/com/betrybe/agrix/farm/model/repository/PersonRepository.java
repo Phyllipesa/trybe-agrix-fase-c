5cc49584d51eb4a4dd68fe1fafd47fcdcc103364
Phyllipe <phylipe.sjc@hotmail.com>
2023-08-25 20:56:43 -0300

    refatoração na PersonRepository.

--- src/main/java/com/betrybe/agrix/farm/model/repository/PersonRepository.java
+++ src/main/java/com/betrybe/agrix/farm/model/repository/PersonRepository.java
@@ -3,11 +3,15 @@ package com.betrybe.agrix.farm.model.repository;
 import com.betrybe.agrix.farm.model.entity.Person;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 /**
  * Person JPA repository.
  */
+
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
-  Optional<Person> findByUsername(String username);
+  UserDetails findByUsername(String username);
 }
