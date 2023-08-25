d775d15227ee329ab64c1cda07d6122e955b59eb
Phyllipe <phylipe.sjc@hotmail.com>
2023-08-25 20:57:18 -0300

    Refatoração na PersonService.

--- src/main/java/com/betrybe/agrix/farm/service/PersonService.java
+++ src/main/java/com/betrybe/agrix/farm/service/PersonService.java
@@ -5,13 +5,16 @@ import com.betrybe.agrix.farm.model.entity.Person;
 import com.betrybe.agrix.farm.model.repository.PersonRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
  * Service layer class for handling persons business logic.
  */
 @Service
-public class PersonService {
+public class PersonService implements UserDetailsService {
 
   private final PersonRepository personRepository;
 
@@ -37,20 +40,25 @@ public class PersonService {
   /**
    * Returns a person for a given username.
    */
-  public Person getPersonByUsername(String username) {
-    Optional<Person> person = personRepository.findByUsername(username);
-
-    if (person.isEmpty()) {
-      throw new PersonNotFoundException();
-    }
-
-    return person.get();
-  }
+  //    public UserDetails getPersonByUsername(String username) {
+  //      Optional<UserDetails> person = personRepository.findByUsername(username);
+  //
+  //      if (person.isEmpty()) {
+  //        throw new PersonNotFoundException();
+  //      }
+  //
+  //      return person.get();
+  //    }
 
   /**
    * Creates a new person.
    */
   public Person create(Person person) {
     return personRepository.save(person);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return personRepository.findByUsername(username);
   }
 }
