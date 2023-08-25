4c22edb28228d43accc1c8ae604f60b3c1e98b52
Phyllipe <phylipe.sjc@hotmail.com>
2023-08-25 20:58:17 -0300

    Implementação da TokenService.

---
+++ src/main/java/com/betrybe/agrix/farm/service/TokenService.java
@@ -0,0 +1,49 @@
+package com.betrybe.agrix.farm.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.betrybe.agrix.farm.model.entity.Person;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * TokenService
+ */
+
+@Service
+public class TokenService {
+  
+  @Value("${api.security.token.secret}")
+  private String secret;
+
+
+  /**
+   * generateToken - Gerador de token.
+   *
+   * @param person dados da pesso.
+   * @return um token.
+   */
+  public String generateToken(Person person) {
+    Algorithm algorithm = Algorithm.HMAC256(secret);
+    return JWT.create()
+        .withIssuer("agrix")
+        .withSubject(person.getUsername())
+        .withExpiresAt(generateExpirationDate())
+        .sign(algorithm);
+  }
+
+  /**
+   * generateExpirationDate - Gera um tempo de vida para o token.
+   *
+   * @return retorna a hora atual com o acrecimo de 2h.
+   */
+  private Instant generateExpirationDate() {
+    return LocalDateTime.now()
+        .plusHours(2)
+        .toInstant(ZoneOffset.of("-03:00"));
+  }
+}
