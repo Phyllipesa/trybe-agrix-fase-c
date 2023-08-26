176b9055b92b410bad94ac43a0b2143133b5b676
Phyllipe <phylipe.sjc@hotmail.com>
2023-08-26 18:29:06 -0300

    Criado um AuthenticationDto.

--- src/main/java/com/betrybe/agrix/farm/controller/dto/AuthenticationDto.java
+++ src/main/java/com/betrybe/agrix/farm/controller/dto/AuthenticationDto.java
@@ -1,4 +1,10 @@
 package com.betrybe.agrix.farm.controller.dto;
 
+/**
+ * Cria um Authentication dto.
+ *
+ * @param username username
+ * @param password password
+ */
 public record AuthenticationDto(String username, String password) {
 }
